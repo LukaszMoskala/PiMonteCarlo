c738b66be78287d404441d29ab47490a11cb46e2
Łukasz Moskała <ukasz_moskala8@tlen.pl>
2019-04-07 00:04:51 +0200

    License

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2019 Łukasz Konrad Moskała
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <iostream>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
@@ -179,6 +195,7 @@ int main(int _args, char** _argv) {
                 al_draw_textf(font, font_color, 0, fh, 0, "PI: %f", pi);
                 al_draw_textf(font, font_color, 0, fh*2, 0, "Error: %f%%", 100.0*(abs_f(pi-M_PI)/M_PI));
                 al_draw_bitmap(preview, preview_x, preview_y, 0);
+                al_draw_text(font, font_color, 0, disp_h-fh, 0, "(C) 2019 Łukasz Moskała");
                 al_flip_display();
                 al_set_target_bitmap(preview);
             }
